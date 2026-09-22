a34f82cb6fd48fcb87e662617b18f5621cc83adf
SAM-2006-GH <shubamgengaje@gmail.com>
2026-09-22 13:26:51 +0530

    Added hello endpoint

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/hello")
 def home():
-    return "Hello from Flask CI/CD!"
+    return "Hello from Jenkins"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000)
